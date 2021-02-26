52a1559ec19c0a024663f7665d83b866ccb9d590
dlwls5201 <dlwls5201@naver.com>
2021-02-27 02:53:54 +0900

    🛠 main 패키지 이동

---
+++ app/src/main/java/mashup/mac/ui/main/MainActivity.kt
@@ -0,0 +1,10 @@
+package mashup.mac.ui.main
+
+import mashup.mac.R
+import mashup.mac.base.BaseActivity
+import mashup.mac.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override var logTag = "MainActivity"
+}
