cfbda850ed4f264f15f03ca5146ad94b5f195972
dlwls5201 <dlwls5201@naver.com>
2021-02-27 09:04:28 +0900

    🧸 Activity 세로 고정 추가

--- app/src/main/java/mashup/mac/base/BaseActivity.kt
+++ app/src/main/java/mashup/mac/base/BaseActivity.kt
@@ -1,9 +1,11 @@
 package mashup.mac.base
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import mashup.mac.util.log.Dlog
 import mashup.mac.util.log.Showlog
 
 abstract class BaseActivity<B : ViewDataBinding>(
@@ -16,8 +18,19 @@ abstract class BaseActivity<B : ViewDataBinding>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setScreenOrientationPortrait()
         binding = DataBindingUtil.setContentView(this, layoutId)
         binding.lifecycleOwner = this
+
+    }
+
+    private fun setScreenOrientationPortrait() {
+        try {
+            //https://gun0912.tistory.com/79
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        } catch (ignore: IllegalStateException) {
+            Dlog.e(ignore.message)
+        }
     }
 
     override fun onResume() {
