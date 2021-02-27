e7e5471f586550ea7c701c2f942a25bd79497f11
dlwls5201 <dlwls5201@naver.com>
2021-02-28 03:15:24 +0900

    🧸 setting 화면 추가

---
+++ app/src/main/java/mashup/mac/ui/setting/SettingActivity.kt
@@ -0,0 +1,10 @@
+package mashup.mac.ui.setting
+
+import mashup.mac.R
+import mashup.mac.base.BaseActivity
+import mashup.mac.databinding.ActivitySampleBinding
+
+class SettingActivity : BaseActivity<ActivitySampleBinding>(R.layout.activity_setting) {
+
+    override var logTag = "SettingActivity"
+}
