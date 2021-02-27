0e23abbb1f6db12f9566f1fe03c8cb586929a3f5
seohui548 <2015111548@swu.ac.kr>
2021-02-28 07:48:40 +0900

    로그인 및 회원가입 UI 작업

--- app/src/main/java/mashup/mac/ui/login/SignUpFragment.kt
+++ app/src/main/java/mashup/mac/ui/login/SignUpFragment.kt
@@ -1,14 +1,16 @@
 package mashup.mac.ui.login
 
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-
-
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import mashup.data.sample.SampleInjection
 import mashup.mac.R
 import mashup.mac.base.BaseFragment
 import mashup.mac.databinding.FragmentSignUpBinding
+import mashup.mac.ui.main.MainActivity
 import mashup.mac.ui.sample.adapter.GithubUserAdapter
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
@@ -26,6 +28,7 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sig
                 SampleInjection.provideRepository()
             )
         ).get(LoginViewModel::class.java)
+
     }
 
     private val userAdapter by lazy { GithubUserAdapter() }
@@ -33,6 +36,77 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sig
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.loginVm = loginViewModel
+
+        binding.pickerYear.maxValue = 2021
+        binding.pickerYear.minValue = 1920
+        binding.pickerYear.wrapSelectorWheel = false
+        binding.pickerYear.value = 2000
+
+        binding.btnNext.setOnClickListener {
+            step++
+            setImageStep(step)
+        }
+    }
+
+    private var step = 1
+
+    private fun setImageStep(step: Int) {
+        when (step) {
+            1 -> {
+                binding.ivStep.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_step1
+                    )
+                )
+                showNickSet()
+            }
+            2 -> {
+                binding.ivStep.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_step2
+                    )
+                )
+                showNickAccess()
+            }
+            3 -> {
+                binding.ivStep.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_step3
+                    )
+                )
+                showNickGender()
+            }
+            else -> {
+                startActivity(
+                    Intent(requireContext(), MainActivity::class.java)
+                )
+                requireActivity().finish()
+            }
+        }
+    }
+
+    private fun showNickSet() {
+        hideAll()
+        binding.nickSet.visibility = View.VISIBLE
+    }
+
+    private fun showNickAccess() {
+        hideAll()
+        binding.nickAccess.visibility = View.VISIBLE
+    }
+
+    private fun showNickGender() {
+        hideAll()
+        binding.nickGender.visibility = View.VISIBLE
+    }
+
+    private fun hideAll() {
+        binding.nickSet.visibility = View.GONE
+        binding.nickAccess.visibility = View.GONE
+        binding.nickGender.visibility = View.GONE
     }
 
 }
