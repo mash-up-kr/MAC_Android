a3844bbee2462d5ec67fd53626215ee765f8f2f6
yuchocopie <pointu341@gmail.com>
2021-03-12 20:27:57 +0900

    web view

--- app/src/main/java/mashup/mac/ui/main/MainActivity.kt
+++ app/src/main/java/mashup/mac/ui/main/MainActivity.kt
@@ -1,10 +1,12 @@
 package mashup.mac.ui.main
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import mashup.data.sample.SampleInjection
 import mashup.mac.R
 import mashup.mac.base.BaseActivity
+import mashup.mac.base.BaseFragment
 import mashup.mac.databinding.ActivityMainBinding
 import mashup.mac.model.Category
 
@@ -25,12 +27,35 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
         binding.mainVm = mainViewModel
 
-        val cue =  arrayListOf<CounselingMapModel>()
-        //x : 210
-        cue.add(CounselingMapModel(500,400,"MapMdoMapMo", Category.관계.title,5))
-        cue.add(CounselingMapModel(210,1300,"제 남친이 좀 이상...", Category.음식.title,1))
-        cue.add(CounselingMapModel(620,1400,"제 남친이 좀 이상...", Category.연애.title,7))
-        cue.add(CounselingMapModel(720,700,"제 남친이 좀 이상...", Category.직업.title,7))
+        val cue = arrayListOf<CounselingMapModel>()
+        cue.add(CounselingMapModel(500, 400, "MapMdoMapMo", Category.관계.title, 5))
+        cue.add(CounselingMapModel(210, 1300, "제 남친이 좀 이상...", Category.음식.title, 1))
+        cue.add(CounselingMapModel(620, 1400, "제 남친이 좀 이상...", Category.연애.title, 7))
+        cue.add(CounselingMapModel(720, 700, "제 남친이 좀 이상...", Category.직업.title, 7))
         binding.customCounselingMap.setCueList(cue)
+
+        mainViewModel.mainListView.observe(this, Observer {
+            val link = when (mainViewModel.mainListView.value) {
+                MainViewModel.CounselingWebView.LIST -> {
+                    counselingList
+                }
+                MainViewModel.CounselingWebView.DETAIL -> {
+                    counselingDetail
+                }
+                else -> counselingDetail
+            }
+            replaceFragment(WebViewFragment.newInstance(link))
+        })
+
     }
+
+    private val counselingList = "https://www.cowcat.live/concerns"
+    private val counselingDetail = "https://www.cowcat.live/concern/edit"
+    private fun replaceFragment(fragment: BaseFragment<*>) {
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.layout_frame_main, fragment)
+            addToBackStack(null)
+        }.commit()
+    }
+
 }
