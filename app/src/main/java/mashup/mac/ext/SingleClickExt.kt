765b20649abe7036af2284b81e54abf77098c345
dlwls5201 <dlwls5201@naver.com>
2021-04-03 20:14:25 +0900

    - 🧸 single click binding adapter 생성

--- app/src/main/java/mashup/mac/ext/SingleClickExt.kt
+++ app/src/main/java/mashup/mac/ext/SingleClickExt.kt
@@ -1,6 +1,15 @@
 package mashup.mac.ext
 
 import android.view.View
+import androidx.databinding.BindingAdapter
+
+
+@BindingAdapter("android:onSingleClick")
+fun View.setOnSingleClick(action: () -> Unit) {
+    setOnSingleClickListener {
+        action.invoke()
+    }
+}
 
 fun View.setOnSingleClickListener(action: (v: View) -> Unit) {
     setOnClickListener(object : OnSingleClickListener() {
