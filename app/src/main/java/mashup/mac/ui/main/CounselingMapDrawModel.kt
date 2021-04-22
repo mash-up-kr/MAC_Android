0af32d7dd278082672ef6ae5c1fdb848243e2377
yuchocopie <pointu341@gmail.com>
2021-04-22 18:20:32 +0900

    add

---
+++ app/src/main/java/mashup/mac/ui/main/CounselingMapDrawModel.kt
@@ -0,0 +1,9 @@
+package mashup.mac.ui.main
+
+data class CounselingMapDrawModel(
+    val id:Int=1,
+    val x:Int=10,
+    val y:Int=10,
+    val category:String = "연애",
+    val distanceKilometer:Int = 5
+)
