185864a929a326816efe8d21e0bb47d1e094f5c2
yuchocopie <pointu341@gmail.com>
2021-04-24 07:48:34 +0900

    🛠 Location model 수정

--- data/src/main/java/mashup/data/model/Location.kt
+++ data/src/main/java/mashup/data/model/Location.kt
@@ -3,21 +3,10 @@ package mashup.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Location(
-    @SerializedName("type")
-    val type: LocationType?,
-    @SerializedName("coordinates")
-    val coordinates: Coordinates?
-) {
-    data class Coordinates(
-        @SerializedName("latitude")
-        val latitude: Double?,
-        @SerializedName("longitude")
-        val longitude: Double?
-    )
-
-    enum class LocationType {
-        Point
-    }
-}
+    @SerializedName("latitude")
+    val latitude: Double?,
+    @SerializedName("longitude")
+    val longitude: Double?
+)
 
 
