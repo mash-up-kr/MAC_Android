3d5e9b58f76732cc9ece7d358684e7c62b0534a0
yuchocopie <pointu341@gmail.com>
2021-04-24 09:58:04 +0900

    🧸 add main counseling recyclerview

--- app/src/main/java/mashup/mac/ui/main/CounselingMapModel.kt
+++ app/src/main/java/mashup/mac/ui/main/CounselingMapModel.kt
@@ -3,8 +3,9 @@ package mashup.mac.ui.main
 data class CounselingMapModel(
     val id: Int = 1,
     val location: Int = 1,
+    val answer: Int = 100,
     val title: String = "제 남친이 좀 이상...",
-    val content: String = "제 남친이 좀 이상...",
+    val description: String = "제 남친이 좀 이상...",
     val date: String = "2021.03.01",
     val category: String = "연애",
     val distanzceKilometer: Int = 5
