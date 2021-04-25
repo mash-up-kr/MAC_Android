1bfbde0d5fcac43661c102973aa173d171b867be
yuchocopie <pointu341@gmail.com>
2021-04-25 22:48:44 +0900

    applocation 초기화 코드 합치기

--- app/src/main/java/mashup/mac/MACApplication.kt
+++ app/src/main/java/mashup/mac/MACApplication.kt
@@ -1,6 +1,7 @@
 package mashup.mac
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import mashup.data.pref.PrefUtil
 
 class MACApplication : Application() {
@@ -8,5 +9,6 @@ class MACApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         PrefUtil.init(this)
+        KakaoSdk.init(this, "5076efb469d1742dd2fb665625c764d7")
     }
 }
