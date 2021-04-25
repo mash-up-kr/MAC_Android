551c2ae23b6fb8b2ace51def1bf46cdcd666fcb5
yuchocopie <pointu341@gmail.com>
2021-04-26 05:45:20 +0900

    🧸 고민작성하기 화면 포커싱 벗어날 경우 키보드 내리기

--- app/src/main/java/mashup/mac/ui/counseling/CounselingWriteActivity.kt
+++ app/src/main/java/mashup/mac/ui/counseling/CounselingWriteActivity.kt
@@ -1,6 +1,10 @@
 package mashup.mac.ui.counseling
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import mashup.data.Injection
 import mashup.mac.R
@@ -11,6 +15,7 @@ import mashup.mac.ext.toast
 import mashup.mac.model.Category
 import mashup.mac.ui.counseling.adapter.AnimalCategoryAdapter
 import mashup.mac.ui.counseling.model.CategoryItem
+
 
 class CounselingWriteActivity :
     BaseActivity<ActivityCounselingWriteBinding>(R.layout.activity_counseling_write) {
@@ -33,6 +38,21 @@ class CounselingWriteActivity :
         loadCategory()
     }
 
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val focusView: View? = currentFocus
+        if (focusView != null) {
+            val rect = Rect()
+            focusView.getGlobalVisibleRect(rect)
+            val x = ev.x.toInt()
+            val y = ev.y.toInt()
+            if (!rect.contains(x, y)) {
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(focusView.windowToken, 0)
+                focusView.clearFocus()
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
     override fun onViewModelSetup() {
         counselingWriteViewModel.eventToast.observeEvent(this) {
             toast(it)
