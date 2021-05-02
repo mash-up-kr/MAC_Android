5449e5bde656ce77389d42bee1b1ca57f49269ee
dlwls5201 <dlwls5201@naver.com>
2021-05-02 12:14:17 +0900

    고민들 시간 데이터 포맷 설정

--- app/src/main/java/mashup/mac/model/CounselingItem.kt
+++ app/src/main/java/mashup/mac/model/CounselingItem.kt
@@ -1,5 +1,9 @@
 package mashup.mac.model
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 data class CounselingItem(
     val id: Int,
     val category: Category,
@@ -7,4 +11,19 @@ data class CounselingItem(
     val content: String = "",
     val date: String = "2020.02.20 오전 08:00",
     val commentCount: Int = 0
-)
+) {
+    val strDate: String
+        get() {
+            val oldFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            oldFormat.timeZone = TimeZone.getTimeZone("KST")
+            val newFormat = SimpleDateFormat("yyyy-MM-dd a hh:mm")
+
+            try {
+                val oldDate = oldFormat.parse(date) ?: return ""
+                return newFormat.format(oldDate)
+            } catch (e: ParseException) {
+                return ""
+            }
+
+        }
+}
